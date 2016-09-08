ef9c0bf13b02208648f4af4d5a310501bf9831e4
Héctor Moreno <hectormoreno@MacBook-Air-de-Hector.local>
2016-09-07 17:07:07 -0700

    Delet Realm Object after logout

--- FbWithCocoapods/FbWithCocoapods/LoggedViewController.swift
+++ FbWithCocoapods/FbWithCocoapods/LoggedViewController.swift
@@ -13,7 +13,7 @@ import FBSDKLoginKit
 
 class LoggedViewController: UIViewController {
     let realm = try! Realm()
-   
+    
 
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var email: UILabel!
@@ -25,11 +25,13 @@ class LoggedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-            let person = realm.objects(Person.self)
+           let person = realm.objects(Person.self)
             name.text = person[0].name
             email.text = person[0].email
             let url = NSURL(string: "https://graph.facebook.com/\(person[0].FbID)/picture?type=large&return_ssl_resources=1")
-            self.imageView.image = UIImage(data: NSData(contentsOfURL: url!)!)
+        self.imageView.image = UIImage(data: NSData(contentsOfURL: url!)!)
+        
+        
         
     }
 
@@ -37,6 +39,11 @@ class LoggedViewController: UIViewController {
         
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         appDelegate.switchBack()
+        
+        try! realm.write {
+            realm.deleteAll()
+        }
+        
         
                 
     }
