1e8241586b8db9eef1a87adeafaa9d2bd099e5a7
Hector Moreno <hmoreno@nearsoft.com>
2016-09-09 10:06:08 -0700

    Change Storyboards Names

--- Nearsoft/Nearsoft/LoginVC.swift
+++ Nearsoft/Nearsoft/LoginVC.swift
@@ -1,9 +1,25 @@
 //
-//  LoginVC.swift
+//  ViewController.swift
 //  Nearsoft
 //
 //  Created by Héctor Moreno on 08/09/16.
 //  Copyright © 2016 Héctor Moreno. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
