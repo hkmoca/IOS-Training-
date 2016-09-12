abb874376cacb02bef84147c904206443806cc71
Hector Moreno <hmoreno@nearsoft.com>
2016-09-11 18:56:02 -0700

    Change icon Image

--- Nearsoft/Nearsoft/ContactsVC.swift
+++ Nearsoft/Nearsoft/ContactsVC.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2016 Héctor Moreno. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Google
+import GoogleSignIn
+
+class ContactsVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    }
