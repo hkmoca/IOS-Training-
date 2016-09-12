b24a618e510fbffd0ae256c8093e7abfbee7738b
Hector Moreno <hmoreno@nearsoft.com>
2016-09-11 23:42:13 -0700

    Create NSRouter

--- Nearsoft/Nearsoft/NSRouter.swift
+++ Nearsoft/Nearsoft/NSRouter.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import Alamofire
+
+enum Router: URLRequestConvertible {
+    
+    static let baseURLString = "http://peopledb-api.azurewebsites.net"
+    
+    case getUsers()
+    
+    var method: Alamofire.Method {
+        switch self {
+        case .getUsers:
+            return .GET
+        }
+    }
+    
+    var path: String {
+        
+        switch self {
+        case .getUsers:
+            return "/api/Employees"
+        }
+    }
+    
+    var URLRequest: NSMutableURLRequest {
+    let URL = NSURL(string: Router.baseURLString)
+    let mutableURLRequest = NSMutableURLRequest(URL: (URL?.URLByAppendingPathComponent(path))!)
+        mutableURLRequest.HTTPMethod = method.rawValue
+    let enconding = Alamofire.ParameterEncoding.URL
+        
+        switch self {
+        case .getUsers:
+            return enconding.encode(mutableURLRequest, parameters: nil).0
+        }
+    }
+}
