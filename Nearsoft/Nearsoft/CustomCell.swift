aff339e352efcdc3059a5a07849b7b46a33163ed
Hector Moreno <hmoreno@nearsoft.com>
2016-09-13 13:18:27 -0700

    Add userNameLabel and emailLabel

--- Nearsoft/Nearsoft/CustomCell.swift
+++ Nearsoft/Nearsoft/CustomCell.swift
@@ -1,9 +1,29 @@
 //
 //  CustomCell.swift
-//  Nearsoft
+//  GithubUsers
 //
-//  Created by Héctor Moreno on 13/09/16.
-//  Copyright © 2016 Héctor Moreno. All rights reserved.
+//  Created by Héctor Moreno on 17/08/16.
+//  Copyright © 2016 Hkapp. All rights reserved.
 //
 
 import Foundation
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var userName: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
