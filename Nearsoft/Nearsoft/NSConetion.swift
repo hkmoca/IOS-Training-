4e2561f73348ee4391946201562c4834be1cfb84
Hector Moreno <hmoreno@nearsoft.com>
2016-09-13 13:06:16 -0700

    Create NSConection with getPeople func

--- Nearsoft/Nearsoft/NSConetion.swift
+++ Nearsoft/Nearsoft/NSConetion.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+import Alamofire
+
+class NSConection {
+    
+    func getPeople(completion: (json: [String: AnyObject]) -> Void, onFailure: (error: NSError) -> Void) {
+        
+        Alamofire.request(NSRouter.getUsers())
+        .responseJSON { response in
+            
+            switch response.result {
+                
+            case.Success(let JSON):
+                completion(json: JSON as! [String: AnyObject])
+                
+            case.Failure(let ERROR):
+                onFailure(error: ERROR)
+            }
+        }
+    }
+
+}
