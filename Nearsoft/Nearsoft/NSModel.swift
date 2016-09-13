9927d2cd2b126893cd69e48ab9f38d1e8cc39b9a
Hector Moreno <hmoreno@nearsoft.com>
2016-09-13 12:56:59 -0700

    Create NSModel with func showPeople

--- Nearsoft/Nearsoft/NSModel.swift
+++ Nearsoft/Nearsoft/NSModel.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import ObjectMapper
+
+class NSModel {
+    func showNSPeople(completion: (people: [User]) -> Void, onFailure: (error: NSError) -> Void){
+        
+        let nsconection = NSConection()
+            nsconection.getPeople({ (json) in
+                if let people: Array<User> = Mapper<User>().mapArray(json)!{
+                    completion(people: people)
+                } else {
+                    print("Nop, Check the code")
+                }
+                },
+                    onFailure: { (error) in
+                    onFailure(error: error)
+        }
+        )
+    }
+}
