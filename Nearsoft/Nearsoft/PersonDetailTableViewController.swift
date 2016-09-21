6282ea85d1a3a0f2aaad26b0c5dc2c9b30e29754
Hector Moreno <hmoreno@nearsoft.com>
2016-09-21 12:29:06 -0700

    Add skype label, Add profileImage

--- Nearsoft/Nearsoft/PersonDetailTableViewController.swift
+++ Nearsoft/Nearsoft/PersonDetailTableViewController.swift
@@ -31,19 +31,23 @@ class PersonDetailTableViewController: UITableViewController {
             akaLabel.text = user.nickname
         }
         
-        //emailLabel.text = user.email
         mobileLabel.text = user.mobile
         clientLabel.text = user.clients
         skype.text = user.skype
+        
         if user.profilePic == "" {
-         //   profileImage.image = UIImage(named: "nearsoft-symbol")
+            profileImage.image = UIImage(named: "nearsoft-symbol")
         }
         
         
         let dayTimePeriodFormatter = NSDateFormatter()
         dayTimePeriodFormatter.dateFormat = "EEEE, dd, MMMM YYYY"
+        if user.birthdate != nil {
         let dateString = dayTimePeriodFormatter.stringFromDate(user.birthdate!)
         birthDate.text = dateString
+        } else {
+        birthDate.text = "Birthdate not available"
+        }
         
         
     }
@@ -77,51 +81,5 @@ class PersonDetailTableViewController: UITableViewController {
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-
+ 
+*/
