f7f97b8c512a56505f7fb38d0f81f8cd1072dc67
Hector Moreno <hmoreno@nearsoft.com>
2016-09-21 10:58:04 -0700

    Create Controller

--- Nearsoft/Nearsoft/PersonDetailTableViewController.swift
+++ Nearsoft/Nearsoft/PersonDetailTableViewController.swift
@@ -10,31 +10,61 @@ import UIKit
 
 class PersonDetailTableViewController: UITableViewController {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var fullName: UILabel!
+    @IBOutlet weak var akaLabel: UILabel!
+    @IBOutlet weak var birthDate: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var mobileLabel: UILabel!
+    @IBOutlet weak var clientLabel: UILabel!
+    
+    var user: User!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        fullName.text = user.fullName
+        if user.aka == "" {
+            akaLabel.text = "Not Available"
+        } else {
+            akaLabel.text = user.aka
+        }
+        
+        //emailLabel.text = user.email
+        mobileLabel.text = user.mobile
+        clientLabel.text = user.clients
+        if user.profilePic == "" {
+         //   profileImage.image = UIImage(named: "nearsoft-symbol")
+        }
+        
+        
+        let dayTimePeriodFormatter = NSDateFormatter()
+        dayTimePeriodFormatter.dateFormat = "EEEE, dd, MMMM YYYY"
+        let dateString = dayTimePeriodFormatter.stringFromDate(user.birthdate!)
+        birthDate.text = dateString
+        
+        
+    }
+    
+    @IBAction func callNumber(sender: AnyObject) {
+        if let url: NSURL = (NSURL(string: "tel://\(user.mobile)")){
+            UIApplication.sharedApplication().openURL(url)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+   
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
 
     /*
@@ -92,4 +122,4 @@ class PersonDetailTableViewController: UITableViewController {
     }
     */
 
-}
+
