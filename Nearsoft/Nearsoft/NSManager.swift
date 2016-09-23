03f20af05bf9495d93f97c258c0b2dc2a6026789
Hector Moreno <hmoreno@nearsoft.com>
2016-09-23 10:37:03 -0700

    Change class name

---
+++ Nearsoft/Nearsoft/NSManager.swift
@@ -0,0 +1,44 @@
+//
+//  NSModel.swift
+//  Nearsoft
+//
+//  Created by Héctor Moreno on 12/09/16.
+//  Copyright © 2016 Héctor Moreno. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+import RealmSwift
+
+class NSManager {
+    let nsconection = NSConection()
+    
+    func showEmployees(completion: (people: [User]) -> Void, onFailure: (error: NSError) -> Void){
+        
+            nsconection.getEmployees({ (json) in
+                if let people: Array<User> = Mapper<User>().mapArray(json)!{
+                    completion(people: people)
+                } else {
+                    print("Nop, Check the code")
+                }
+                }, onFailure: { (error) in
+                    onFailure(error: error)
+                }
+        )
+    }
+    
+    func showInterns(completion: (interns: [User]) -> Void, onFailure: (error: NSError) -> Void) {
+    
+        nsconection.getInterns({ (json) in
+                if let interns: Array<User> = Mapper<User>().mapArray(json)!{
+                    completion(interns: interns)
+                } else {
+                    print("Something´s wrong with the intens")
+                }
+                }, onFailure: { (error) in
+                    onFailure(error: error)
+                }
+                )
+    }
+    
+}
