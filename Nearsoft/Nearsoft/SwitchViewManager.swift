2b6c812c63014c5af4f44bea4cd8124c00d78422
Hector Moreno <hmoreno@nearsoft.com>
2016-09-23 12:37:37 -0700

    Create SwitchViewManager

--- Nearsoft/Nearsoft/SwitchViewManager.swift
+++ Nearsoft/Nearsoft/SwitchViewManager.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+import UIKit
+class SwitchViewManager {
+    
+ static let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+
+   static func switchToLogged() {
+        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+        let nav = storyboard.instantiateViewControllerWithIdentifier("Main")
+        appDelegate.window?.rootViewController = nav
+        
+    }
+    
+   static func switchToLogin(){
+        let storyboard = UIStoryboard.init(name: "Login", bundle: nil)
+        let nav = storyboard.instantiateViewControllerWithIdentifier("Login")
+        appDelegate.window?.rootViewController = nav
+        
+    }
+
+
+}
