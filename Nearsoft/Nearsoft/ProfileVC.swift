fffa712f7f94c0c5df6990ab0b840fd463c1ac6b
Héctor Moreno <hmoreno@nearsoft.com>
2016-09-27 15:39:05 -0700

    Change ViewController for TableViewcontroller and add labels on table view cells

--- Nearsoft/Nearsoft/ProfileVC.swift
+++ Nearsoft/Nearsoft/ProfileVC.swift
@@ -1,39 +1,57 @@
-//
-//  ViewController.swift
-//  Nearsoft
-//
-//  Created by Héctor Moreno on 08/09/16.
-//  Copyright © 2016 Héctor Moreno. All rights reserved.
-//
 
 import UIKit
-
-import GoogleSignIn
-import RealmSwift
 import AlamofireImage
+import RealmSwift
+import GoogleSignIn
 
-
-
-class ProfileVC: UIViewController {
-    let realm = try! Realm()
+class ProfileVC: UITableViewController {
     
-    @IBOutlet weak var profileImage: UIImageView!
-    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var profilePic: UIImageView!
+    @IBOutlet weak var fullName: UILabel!
+    @IBOutlet weak var birthDate: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
-
+    @IBOutlet weak var mobileLabel: UILabel!
+    let realm = try! Realm()
+    var user = User()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        profilePic.layer.borderWidth = 1
+        profilePic.layer.masksToBounds = false
+        profilePic.layer.borderColor = UIColor.orangeColor().CGColor
+        profilePic.layer.cornerRadius = profilePic.frame.height/2
+        profilePic.clipsToBounds = true
+        
         let persons = realm.objects(User.self)
         let person = persons[0] as User
-            nameLabel.text = person.fullName
-            emailLabel.text = person.email
+        fullName.text = person.fullName
+        emailLabel.text = person.email
         let profilePicURL =  NSURL(string: "\(person.profilePic)")
-        self.profileImage.af_setImageWithURL(profilePicURL!)
-           
-    }
+        self.profilePic.af_setImageWithURL(profilePicURL!)
 
-       @IBAction func didTapSignOut(sender: AnyObject) {
+        
+        if user.profilePic == "" {
+            profilePic.image = UIImage(named: "nearsoft-symbol")
+        } else {
+            profilePic.af_setImageWithURL(profilePicURL!)
+        }
+        
+        
+        let dayTimePeriodFormatter = NSDateFormatter()
+        dayTimePeriodFormatter.dateFormat = "EEEE, dd, MMMM YYYY"
+        
+        if user.birthdate != nil {
+            let dateString = dayTimePeriodFormatter.stringFromDate(user.birthdate!)
+            birthDate.text = dateString
+        } else {
+            birthDate.text = "Birthdate not available"
+        }
+    }
+    
+    
+    @IBAction func didTapSignOut(sender: AnyObject) {
         GIDSignIn.sharedInstance().signOut()
         
         try! realm.write {
@@ -42,6 +60,14 @@ class ProfileVC: UIViewController {
         
         SwitchViewManager.switchToLogin()
     }
-
 }
+
+
+// MARK: - Table view data source
+
+func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    // #warning Incomplete implementation, return the number of sections
+    return 1
+}
+
 
